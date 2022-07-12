db6af82b504eef3c1b604adc4003dce44154bb12
JasonSchneider8 <36863375+JasonSchneider8@users.noreply.github.com>
2022-07-12 00:14:16 -0400

    Initial Commit

---
+++ crypto-market-tracker-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  crypto-market-tracker-ios
+//
+//  Created by Jason Schneider on 7/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ crypto-market-tracker-ios/crypto_market_tracker_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_market_tracker_iosApp.swift
+//  crypto-market-tracker-ios
+//
+//  Created by Jason Schneider on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct crypto_market_tracker_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
